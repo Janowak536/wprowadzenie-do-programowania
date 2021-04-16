87bbfd41b479c4e4356793210390f18292aac0f6
Janowak536 <janowak536@gmail.com>
2021-04-16 15:54:17 +0200

    Add project files.

---
+++ wzorki/wzorki3/wzorki3/wzorki3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wzorki3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
