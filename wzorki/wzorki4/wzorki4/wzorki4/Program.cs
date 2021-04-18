325d4d6ca39d7c7b24d29707df6afc1172f51983
Janowak536 <janowak536@gmail.com>
2021-04-18 13:48:14 +0200

    gotowe zadanie cpttrn3

--- wzorki/wzorki4/wzorki4/wzorki4/Program.cs
+++ wzorki/wzorki4/wzorki4/wzorki4/Program.cs
@@ -22,28 +22,19 @@ namespace wzorki3
             for (int j = 0; j < a; j++)              // koniec pętli po zakończeniu obiektów
             {
 
-                for (int i = 0; i <= wymiary[j, 0] ; i++)     //rozpoczęcie pętli piszącej wzorek pisanie pojedynczego wiersza
-                {
-                    for (int k = 0; k < wymiary[j,1]; k++)
+                for (int i = 0; i <= (wymiary[j, 0] *3); i++)     //tutaj moze dodac 1 albo 2
+                {                                               // i to pisanie wierszy  długość
+                    for (int k = 0; k <= (wymiary[j,1]*3); k++)   // szerokość
                     {
- 
-                        if (i == 0 || i- 1>= wymiary[j, 0] )
+                        if (i==0 ||i%3==0||i== ((wymiary[j, 0] * 3)+1)||k==0||k%3==0||k==((wymiary[j, 1] * 3)+1))
                         {
-                            Console.Write($"***");
+                            Console.Write("*");
                         }
-                        if (k == 0)
-                            Star();
-                        if (i != 0)
-                            Chain();
+                        else
+                            Console.Write(".");
                     }
                     Console.WriteLine();
-
                 }
-                for (int i = 0; i < wymiary[j,1]; i++)
-                {
-                    Console.Write($"***");
-                }
-                Console.Write("*");
                 Console.WriteLine();
                 Console.WriteLine();
             }
