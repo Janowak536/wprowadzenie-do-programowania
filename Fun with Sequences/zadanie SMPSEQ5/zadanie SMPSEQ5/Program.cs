4f794695f7ad0c73dc2d96cf97da0669e25c15b3
Janowak536 <janowak536@gmail.com>
2021-04-19 22:07:23 +0200

    zrobione zadanie

---
+++ Fun with Sequences/zadanie SMPSEQ5/zadanie SMPSEQ5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace zadanie_SMPSEQ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string liczba1 = Console.ReadLine();
+            string liczby1 = Console.ReadLine();
+            string liczba2 = Console.ReadLine();
+            string liczby2 = Console.ReadLine();
+            int[] tablica1 = Array.ConvertAll<string, int>(liczby1.Split(" "), int.Parse);
+            int[] tablica2 = Array.ConvertAll<string, int>(liczby2.Split(" "), int.Parse);
+
+            for (int i = 0; i < tablica1.Length; i++)
+            {
+                if (tablica1[i] == tablica2[i])
+                {
+                    if (i == 0) continue;
+                    if (i == tablica1.Length)
+                        Console.Write(i + 1);
+
+                    else
+                    {
+                        Console.Write((i + 1) + " ");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
