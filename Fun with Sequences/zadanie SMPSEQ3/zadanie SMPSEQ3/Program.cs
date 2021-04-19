80ad598fcabce1abcbe8248161385f74b45a30de
Janowak536 <janowak536@gmail.com>
2021-04-19 11:46:50 +0200

    zrobione zadania

---
+++ Fun with Sequences/zadanie SMPSEQ3/zadanie SMPSEQ3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace zadanie_SMPSEQ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+            string d = Console.ReadLine();
+            string[] tablicab = b.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);  // tak trzeba
+            string[] tablicad = d.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);  // tak trzeba
+            for (int i = 0; i < tablicab.Length; i++)
+            {
+                bool found = false;
+                for (int j = 0; j < tablicad.Length; j++)
+                {
+                    if (tablicab[i] == tablicad[j])
+                    {
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    Console.Write(tablicab[i] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Fun with Sequences/zadanie SMPSEQ4/zadanie SMPSEQ4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace zadanie_SMPSEQ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+            string d = Console.ReadLine();
+            string[] tablicab = b.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);  // tak trzeba
+            string[] tablicad = d.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);  // tak trzeba
+            for (int i = 0; i < tablicab.Length; i++)
+            {
+                bool found = true;
+                for (int j = 0; j < tablicad.Length; j++)
+                {
+                    if (tablicab[i] == tablicad[j])
+                    {
+                        found = false;
+                    }
+                }
+                if (!found)
+                {
+                    Console.Write(tablicab[i] + " ");
+                }
+            }
+        }
+    }
+}
+
