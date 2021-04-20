733248efcefabc257d424598be1a769ab759f053
Janowak536 <janowak536@gmail.com>
2021-04-20 16:40:31 +0200

    zrobione zadanie

---
+++ probny sprawdzian/probny sprawdzian zad 1/probny sprawdzian zad 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace probny_sprawdzian_zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine(); //to usunąć
+            int a = Int32.Parse(n);  // tutaj zmienić żeby było int a = n i gotowe albo zmienić a na n
+            if (a < 3)
+            {
+                Console.WriteLine("error");
+            }
+            if(a%2==0)
+            {
+                a--;
+            }
+            for (int i = 0; i < a; i++)  //wiersze
+            {
+                for (int j = 0; j < a; j++) //długość
+                {
+                    if ( i>j|| a-i<=j)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
