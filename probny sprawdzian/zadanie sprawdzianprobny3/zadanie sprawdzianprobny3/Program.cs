985fa19cbb5c6183628485277b234e4998cd9f64
Janowak536 <janowak536@gmail.com>
2021-04-21 13:31:10 +0200

    gotowy zrobiony program

--- probny sprawdzian/zadanie sprawdzianprobny3/zadanie sprawdzianprobny3/Program.cs
+++ probny sprawdzian/zadanie sprawdzianprobny3/zadanie sprawdzianprobny3/Program.cs
@@ -1,134 +1,35 @@
 ﻿using System;
 
-namespace zadanie_sprawdzianprobny3
+namespace exceptions
 {
     class Program
     {
         static void Main()
         {
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            string c = Console.ReadLine();
             try
             {
-                Stringi(a, b, c);
-            
-                try
-                {
-                    overflow(a, b, c);
-                      try
-                      {
-                             
-                             pars(a, b, c);
-                      }
-                      catch (FormatException z)
-                      {
-
-                      Console.WriteLine("format exception, exit", z);
-                        return;
-                      }
-                     
-                }
-                catch (OverflowException g)
-                {
-
-                Console.WriteLine("overflow exception, exit", g);
-                    return;
-                }
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine("argument exception, exit", e);
-                return;
-            }
-            suma(a, b, c);
-        }
-        static void suma(string a, string b, string c)
-        {
-            int d = Int32.Parse(a);
-            int e = Int32.Parse(b);
-            int f = Int32.Parse(c);
-
-            Console.WriteLine(d+e+f);
-
-        }
-
-
-        static void Stringi(string a, string b, string c)
-        {
-
-            if (a == "" || b == "" || c == "")
-            {
-                throw new ArgumentException();
-            }
-
-        }
-
-        static void pars(string a, string b, string c)
-        {
-            
-            try
-            {
-                Int32.Parse(a);
-
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+                Console.WriteLine(checked(a + b + c));
             }
             catch (FormatException)
             {
-                throw new FormatException();
+                Console.WriteLine("format exception, exit");
             }
-            try
+            catch (ArgumentException)
             {
-                 Int32.Parse(b);
-
-            }
-            catch (FormatException)
-            {
-                throw new FormatException();
-            }
-            try
-            {
-                Int32.Parse(c);
-
-            }
-            catch (FormatException)
-            {
-                throw new FormatException();
-            }
-          
-
-
-        }
-
-        static void overflow(string a, string b, string c)
-        {
-           
-            try
-            {
-                 Int32.Parse(a);
-                
+                Console.WriteLine("argument exception, exit");
             }
             catch (OverflowException)
             {
-                throw new OverflowException();
+                Console.WriteLine("overflow exception, exit");
             }
-            try
+            catch (Exception)
             {
-                Int32.Parse(b);
-                
+                Console.WriteLine("non supported exception, exit");
             }
-            catch (OverflowException)
-            {
-                throw new OverflowException();
-            }
-            try
-            {
-                Int32.Parse(c);
-                
-            }
-            catch (OverflowException)
-            {
-                throw new OverflowException();
-            }
+
         }
     }
 }
