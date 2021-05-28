6a22d6056482da751ae7a95c472656fa9457cfe2
Janowak536 <janowak536@gmail.com>
2021-05-28 09:54:48 +0200

    stos i kolejka

---
+++ Kolejka i stos/Kolejka i stos/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kolejka_i_stos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stos = new Stack<string>();
+            stos.Push("1");
+            stos.Push("2");
+            stos.Push("3");
+            stos.Push("4");
+
+            Queue<string> kolejka = new Queue<string>();
+            kolejka.Enqueue("a");
+            kolejka.Enqueue("b");
+            kolejka.Enqueue("c");
+            kolejka.Enqueue("d");
+
+            foreach(string sztuka in stos)
+            {
+                Console.WriteLine(sztuka);
+            }
+
+            Console.WriteLine();
+
+
+            foreach(var pozycja in kolejka)
+            {
+                Console.WriteLine(pozycja);
+            }
+
+
+            kolejka.Enqueue(stos.Pop());
+            kolejka.Enqueue(stos.Pop());
+            
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var pozycja in kolejka)
+            {
+                Console.WriteLine(pozycja);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (string sztuka in stos)
+            {
+                Console.WriteLine(sztuka);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(stos.Peek());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(kolejka.Peek());
+
+
+
+
+
+        }
+    }
+}
