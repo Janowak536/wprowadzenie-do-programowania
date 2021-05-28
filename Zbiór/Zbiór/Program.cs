8f1e5958517a7efc8bab4b747f9d99bdc204c129
Janowak536 <janowak536@gmail.com>
2021-05-28 14:14:19 +0200

    .

---
+++ Zbiór/Zbiór/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Zbiór
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "1", "3", "5", "2", "7", "8" };
+
+
+            string[] array2 = { "2", "1", "5", "3", "9", "6" };
+
+            foreach(var number in array1)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
