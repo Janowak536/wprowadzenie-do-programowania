56dc2822c52adfcce15879852f2bfa5c436052f6
Janowak536 <janowak536@gmail.com>
2021-05-31 16:45:53 +0200

    gotowe zadanie

--- RKS- RK SORTING/RKS- RK SORTING/Program.cs
+++ RKS- RK SORTING/RKS- RK SORTING/Program.cs
@@ -1,30 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
+using System.Collections;
 
 namespace RKS__RK_SORTING
 {
-    public static class RKS
+    public static class Program
     {
-        public static void Logic(int Length,int Max, string outputfull)
+        static void Main()
         {
-           string[] output = outputfull.Split(' ');
+            string stringessumption = Console.ReadLine();
+            string stringnumbers = Console.ReadLine();
+            int[] assumption = Array.ConvertAll<string, int>(stringessumption.Split(" "), int.Parse);
+            int[] numbers = Array.ConvertAll<string, int>(stringnumbers.Split(" "), int.Parse);
+            int Length = assumption[0];
+            int Max = assumption[1];
 
-            List<int> numbers = new List<int>();
-
-
-            return output;
-
-
-        }
-        public static void Main(string[] args)
-        {
-            string[] input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            int Length = int.Parse(input[0]);
-            int Max = int.Parse(input[1]);
-            string output = Console.ReadLine();
-
+            ArrayList output = new ArrayList();
+            var DCT = new Dictionary<int, int>();
+            for (int i = 0; i < Length; i++)
+            {
+                if(DCT.ContainsKey(numbers[i]) == false)
+                {
+                    DCT.Add(numbers[i], 1);
+                }
+                else
+                {
+                    int temp = DCT[numbers[i]];
+                    DCT[numbers[i]] = temp + 1;
+                }
+            }
+            var sortedDict = from wartosc in DCT orderby wartosc.Value descending select wartosc;
+            Dictionary<int, int>.ValueCollection wartosci = DCT.Values;
+            Dictionary<int, int>.KeyCollection klucze = DCT.Keys;
+            foreach(var element in sortedDict)
+            {
+                int WR = element.Key;
+                int value = element.Value;
+                for (int i = 0; i < value; i++)
+                {
+                    output.Add(WR);
+                    output.Add(" ");
+                }
+            }
+            output.RemoveAt((output.Count)-1);
+            for (int i = 0; i < (Length*2)-1; i++)
+            {
+                Console.Write(output[i]);
+            }
         }
     }
 }
